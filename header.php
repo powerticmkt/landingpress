70106130580fc15e541a0c6bd2b62e31505a0c69
Luiz Eduardo Oliveira Fonseca <luizeof@me.com>
2016-06-21 09:12:29 -0300

    Iniciando .....

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta name="viewport" content="width=device-width" />
+		<title><?php wp_title(); ?></title>
+		<?php wp_head(); ?>
+	</head>
+
+<body <?php body_class(); ?>>
+<main>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php get_header();  ?>
+
+			<?php if ( have_posts() ) : ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+						<?php the_content();?>
+
+				<?php endwhile; ?>
+
+			<?php else : ?>
+
+				<article>
+					<h1>Nenhuma informação na página</h1>
+				</article>
+
+			<?php endif; ?>
+
+<?php get_footer(); ?>
