0de996793331a4b4780f9cead0cabc0bf4ea8192
Luiz Eduardo Oliveira Fonseca <luizeof@me.com>
2016-07-05 05:56:22 -0300

    bugfix

--- footer.php
+++ footer.php
@@ -1,12 +1,24 @@
 
     <?php wp_footer(); ?>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
-    <script type="text/javascript" src="https://raw.githubusercontent.com/kswedberg/jquery-smooth-scroll/master/jquery.smooth-scroll.js"></script>
 
     <script type="text/javascript">
       new WOW().init();
-      $( document ).ready(function() {
-          $('a').smoothScroll();
+      $(document).ready(function() {
+        $(function() {
+          $('a[href*="#"]:not([href="#"])').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+              var target = $(this.hash);
+              target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+              if (target.length) {
+                $('html, body').animate({
+                  scrollTop: target.offset().top
+                }, 1000);
+                return false;
+              }
+            }
+          });
+        });
       });
     </script>
   </body>
