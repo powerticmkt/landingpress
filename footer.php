fc7657df5a01c62135d444ad7dfee6114af4b7c8
Luiz Eduardo Oliveira Fonseca <luizeof@me.com>
2016-07-05 05:54:13 -0300

    Ajustando o Scroll de âncoras

--- footer.php
+++ footer.php
@@ -1,8 +1,13 @@
 
     <?php wp_footer(); ?>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+    <script type="text/javascript" src="https://raw.githubusercontent.com/kswedberg/jquery-smooth-scroll/master/jquery.smooth-scroll.js"></script>
+
     <script type="text/javascript">
       new WOW().init();
+      $( document ).ready(function() {
+          $('a').smoothScroll();
+      });
     </script>
   </body>
 </html>
