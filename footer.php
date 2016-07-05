b3546af67bc30e6003bc4136e57df9e45c170fbb
Luiz Eduardo Oliveira Fonseca <luizeof@me.com>
2016-07-05 05:41:28 -0300

    Incluindo o wow e o animate css

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
 
     <?php wp_footer(); ?>
-
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+    <script type="text/javascript">
+      new WOW().init();
+    </script>
   </body>
 </html>
