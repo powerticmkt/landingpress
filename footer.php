c1f94d603ca9b9ae93dba5dec3ec1302fe050a28
Luiz Eduardo Oliveira Fonseca <luizeof@me.com>
2016-07-05 06:00:16 -0300

    bugfixes

--- footer.php
+++ footer.php
@@ -4,9 +4,9 @@
 
     <script type="text/javascript">
       new WOW().init();
-      $(document).ready(function() {
-        $(function() {
-          $('a[href*="#"]:not([href="#"])').click(function() {
+      jQuery(document).ready(function() {
+        jQuery(function() {
+          jQuery('a[href*="#"]:not([href="#"])').click(function() {
             if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
               var target = $(this.hash);
               target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
