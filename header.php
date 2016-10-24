14ffe29a06396adde5a1e0e0614235a73e34588b
Luiz Eduardo Oliveira Fonseca <luizeof@me.com>
2016-10-24 20:44:29 -0200

    bugfixes

--- header.php
+++ header.php
@@ -4,10 +4,7 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width" />
 		<title><?php wp_title(); ?></title>
-		<link rel='stylesheet' id='style-css' href="https://necolas.github.io/normalize.css/latest/normalize.css"/>
 		<?php wp_head(); ?>
 	</head>
 
 <body <?php body_class(); ?>>
-
-<?php if ( function_exists( 'gtm4wp_the_gtm_tag' ) ) { gtm4wp_the_gtm_tag(); } ?>
