a661468b7380d99530cf401a79f6c145a3374ea4
Luiz Eduardo Oliveira Fonseca <luizeof@me.com>
2016-10-25 21:13:29 -0200

    Bugfixes

--- functions.php
+++ functions.php
@@ -10,12 +10,6 @@ License: GNU General Public License
 */
 
 
-function elementornaked_customize_register( $wp_customize ) {
-  // Do stuff with $wp_customize, the WP_Customize_Manager object.
-}
-add_action( 'customize_register', 'elementornaked_customize_register' );
-
-
 function register_my_menus() {
   register_nav_menus(
     array(
@@ -25,6 +19,22 @@ function register_my_menus() {
   );
 }
 add_action( 'init', 'register_my_menus' );
+
+
+
+if (function_exists('register_sidebar')) {
+
+	register_sidebar(array(
+		'name' => 'Widgetized Area',
+		'id'   => 'widgetized-area',
+		'description'   => 'This is a widgetized area.',
+		'before_widget' => '<div id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</div>',
+		'before_title'  => '<h4>',
+		'after_title'   => '</h4>'
+	));
+
+}
 
 
 ?>
