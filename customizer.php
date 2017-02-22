9002c5204acac14493fdc5f8ff02599700bad526
Luiz Eduardo Oliveira Fonseca <luizeof@me.com>
2017-02-22 06:07:20 -0300

    Docs and Comments

--- customizer.php
+++ customizer.php
@@ -4,7 +4,7 @@
     -------------------------------------------------------- */
     function elementor_puzzle_header_custom($wp_customize){
         $wp_customize->add_section('elementor_pzl_header', array(
-            'title'     => __('Cabeçalho do website', 'elementor-puzzle'),
+            'title'     => __('Website Header', 'elementor-puzzle'),
             'priority'  => 140
         ));
         // Exibir Header?
@@ -13,7 +13,7 @@
             'priority'  => 1
         ));
         $wp_customize->add_control(new WP_Customize_Control($wp_customize, 'pzl_head_show', array(
-            'label'     => __('Exibir Cabeçalho?', 'elementor-puzzle'),
+            'label'     => __('Show Header?', 'elementor-puzzle'),
             'section'   => 'elementor_pzl_header',
             'settings'  => 'pzl_head_show',
             'type'      => 'select',
@@ -25,7 +25,7 @@
             'priority'  => 4
         ));
         $wp_customize->add_control(new WP_Customize_Control($wp_customize, 'pzl_head_shortcode', array(
-            'label'     => __('Shortcode', 'elementor-puzzle'),
+            'label'     => __('Header Shortcode', 'elementor-puzzle'),
             'section'   => 'elementor_pzl_header',
             'settings'  => 'pzl_head_shortcode'
         )));
@@ -35,54 +35,54 @@
 
     /* Menu Shortcode
     -------------------------------------------------------- */
-    function elementor_puzzle_menu_custom($wp_customize){
-        $wp_customize->add_section('elementor_pzl_menu', array(
-            'title'     => __('Menu do website', 'elementor-puzzle'),
+    function elementor_puzzle_header_menu_custom($wp_customize){
+        $wp_customize->add_section('elementor_pzl_hmenu', array(
+            'title'     => __('Website Header Menu', 'elementor-puzzle'),
             'priority'  => 140
         ));
-        // Exibir Header?
-        $wp_customize->add_setting('pzl_menu_show', array(
+        // Exibir Header Menu?
+        $wp_customize->add_setting('pzl_hmenu_show', array(
             'default'   => 'No',
             'priority'  => 1
         ));
-        $wp_customize->add_control(new WP_Customize_Control($wp_customize, 'pzl_menu_show', array(
-            'label'     => __('Exibir Menu?', 'elementor-puzzle'),
-            'section'   => 'elementor_pzl_menu',
-            'settings'  => 'pzl_menu_show',
+        $wp_customize->add_control(new WP_Customize_Control($wp_customize, 'pzl_hmenu_show', array(
+            'label'     => __('Show Header Menu?', 'elementor-puzzle'),
+            'section'   => 'elementor_pzl_hmenu',
+            'settings'  => 'pzl_hmenu_show',
             'type'      => 'select',
-            'choices'   => array('No' => 'Não', 'Yes' => 'Sim')
+            'choices'   => array('No' => __('No', 'elementor-puzzle'), 'Yes' => __('Yes', 'elementor-puzzle'))
         )));
-        // Shortcode do Header
-        $wp_customize->add_setting('pzl_menu_shortcode', array(
+        // Shortcode do Header Menu
+        $wp_customize->add_setting('pzl_hmenu_shortcode', array(
             'default'   => '',
             'priority'  => 4
         ));
-        $wp_customize->add_control(new WP_Customize_Control($wp_customize, 'pzl_menu_shortcode', array(
+        $wp_customize->add_control(new WP_Customize_Control($wp_customize, 'pzl_hmenu_shortcode', array(
             'label'     => __('Shortcode', 'elementor-puzzle'),
-            'section'   => 'elementor_pzl_menu',
-            'settings'  => 'pzl_menu_shortcode'
+            'section'   => 'elementor_pzl_hmenu',
+            'settings'  => 'pzl_hmenu_shortcode'
         )));
-        // Shortcode do Header
+        // Posição do Header Menu
         $wp_customize->add_setting('pzl_menu_position', array(
             'default'   => 'After',
             'priority'  => 5
         ));
-        $wp_customize->add_control(new WP_Customize_Control($wp_customize, 'pzl_menu_position', array(
+        $wp_customize->add_control(new WP_Customize_Control($wp_customize, 'pzl_hmenu_position', array(
             'label'     => __('Posição do Menu?', 'elementor-puzzle'),
-            'section'   => 'elementor_pzl_menu',
-            'settings'  => 'pzl_menu_position',
+            'section'   => 'elementor_pzl_hmenu',
+            'settings'  => 'pzl_hmenu_position',
             'type'      => 'select',
-            'choices'   => array('Before' => 'Antes do Cabeçalho', 'After' => 'Após o Cabeçalho')
+            'choices'   => array('Before' => __('Before Header', 'elementor-puzzle'), 'After' => __('After Header', 'elementor-puzzle'))
         )));
     }
-    add_action( 'customize_register', 'elementor_puzzle_menu_custom' );
+    add_action( 'customize_register', 'elementor_puzzle_header_menu_custom' );
 
 
     /* Footer Shortcode
     -------------------------------------------------------- */
     function elementor_puzzle_footer_custom($wp_customize){
         $wp_customize->add_section('elementor_pzl_footer', array(
-            'title'     => __('Rodapé do website', 'elementor-puzzle'),
+            'title'     => __('Footer', 'elementor-puzzle'),
             'priority'  => 140
         ));
         // Exibir Header?
@@ -91,7 +91,7 @@
             'priority'  => 1
         ));
         $wp_customize->add_control(new WP_Customize_Control($wp_customize, 'pzl_footer_show', array(
-            'label'     => __('Exibir Rodapé?', 'elementor-puzzle'),
+            'label'     => __('Show Footer?', 'elementor-puzzle'),
             'section'   => 'elementor_pzl_footer',
             'settings'  => 'pzl_head_footer',
             'type'      => 'select',
@@ -103,7 +103,7 @@
             'priority'  => 4
         ));
         $wp_customize->add_control(new WP_Customize_Control($wp_customize, 'pzl_footer_shortcode', array(
-            'label'     => __('Shortcode', 'elementor-puzzle'),
+            'label'     => __('Footer Shortcode', 'elementor-puzzle'),
             'section'   => 'elementor_pzl_footer',
             'settings'  => 'pzl_footer_shortcode'
         )));
--- functions.php
+++ functions.php
@@ -17,41 +17,15 @@ function naked_menu_init() {
   register_nav_menus(
     array(
       'header-menu' => __( 'Header Menu', 'elementor-naked'),
-      'extra-menu' => __( 'Extra Menu', 'elementor-naked' )
+      'extra-menu' => __( 'Footer Menu', 'elementor-naked' )
     )
   );
 }
 add_action( 'init', 'naked_menu_init' );
 
-add_theme_support( 'automatic-feed-links' );
-
-//add_theme_support( 'title-tag' );
+add_theme_support( 'title-tag' );
 
 add_theme_support( "post-thumbnails" );
-
-//add_theme_support( "custom-header");
-
-//add_theme_support( "custom-background");
-
-function naked_setup() {
-  if ( ! isset( $content_width ) ) $content_width = 900;
-}
-add_action( 'after_setup_theme', 'naked_setup' );
-
-add_editor_style();
-
-add_action( 'widgets_init', 'naked_widgets_init' );
-function naked_widgets_init() {
-    register_sidebar( array(
-        'name' => __( 'Main Sidebar', 'elementor-naked' ),
-        'id' => 'sidebar-1',
-        'description' => __( 'Widgets in this area will be shown on all posts and pages.', 'elementor-naked' ),
-        'before_widget' => '<li id="%1$s" class="widget %2$s">',
-  	    'after_widget'  => '</li>',
-  	    'before_title'  => '<h2 class="widgettitle">',
-  	    'after_title'   => '</h2>',
-    ) );
-}
 
 // Run updater
 require 'plugin-update-checker/plugin-update-checker.php';
