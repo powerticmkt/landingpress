ba1234fec0c35c0180e8add125fa62e54336575e
Luiz Eduardo Oliveira Fonseca <luizeof@me.com>
2017-03-16 20:35:22 -0300

    Bugfixes

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
         <?php if (get_theme_mod('pzl_fmenu_position') == 'Before') : ?>
           <?php echo do_shortcode( get_theme_mod("pzl_fmenu_shortcode") ); ?>
         <?php endif; ?>
-      <?php ?>
+      <?php endif; ?>
 
         <?php if (get_theme_mod('pzl_footer_show') == 'Yes') : ?>
           <?php echo do_shortcode( get_theme_mod("pzl_footer_shortcode") ); ?>
