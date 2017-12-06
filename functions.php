039763108540d8a276d82713b41ff7601504538d
Luiz Eduardo Oliveira Fonseca <luizeof@gmail.com>
2017-12-06 21:07:58 -0200

    Ajustes e correções

--- functions.php
+++ functions.php
@@ -11,16 +11,28 @@ License: GNU General Public License
 
 require_once "customizer.php";
 
-wp_enqueue_style( 'style', get_stylesheet_uri() );
-
 add_theme_support( 'title-tag' );
 
 add_theme_support( "post-thumbnails" );
 
+
+
+add_action( 'after_setup_theme', 'register_landingpress_menus' );
+function register_landingpress_menus() {
+  register_nav_menu( 'primary_menu', __( 'Principal', 'landingpress' ) );
+  register_nav_menu( 'secondary_menu', __( 'Secundário', 'landingpress' ) );
+  register_nav_menu( 'footer_menu', __( 'Rodapé', 'landingpress' ) );
+  register_nav_menu( 'sidebar_menu', __( 'Barra Lateral', 'landingpress' ) );
+}
+
+
+
+
+
 // Run updater
 require 'plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-    'https://github.com/powertic/landingpress/',
+    'https://github.com/powerticmkt/landingpress/',
     __FILE__,
     'landingpress'
 );
--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" />
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?> >
