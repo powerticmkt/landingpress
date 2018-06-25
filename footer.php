7ff75b08c0785d2453b2969b8026a4735553892c
Luiz Eduardo de Oliveira Fonseca <luiz@powertic.com>
2018-06-25 11:57:24 -0300

    2.0.0-beta

--- footer.php
+++ footer.php
@@ -1,22 +1,6 @@
 
   </div>
 
-  		<?php if (get_theme_mod('pzl_fmenu_show') == 'Yes') : ?>
-        <?php if (get_theme_mod('pzl_fmenu_position') == 'Before') : ?>
-          <?php echo do_shortcode( get_theme_mod("pzl_fmenu_shortcode") ); ?>
-        <?php endif; ?>
-      <?php endif; ?>
-
-        <?php if (get_theme_mod('pzl_footer_show') == 'Yes') : ?>
-          <?php echo do_shortcode( get_theme_mod("pzl_footer_shortcode") ); ?>
-        <?php endif; ?>
-
-      <?php if (get_theme_mod('pzl_fmenu_show') == 'Yes') : ?>
-        <?php if (get_theme_mod('pzl_fmenu_position') == 'After') : ?>
-    			<?php echo do_shortcode( get_theme_mod("pzl_fmenu_shortcode") ); ?>
-    		<?php endif; ?>
-      <?php endif; ?>
-
       <?php wp_footer(); ?>
 
   </body>
--- header.php
+++ header.php
@@ -7,26 +7,4 @@
 	</head>
 	<body <?php body_class(); ?> >
 
-	<div class="header-wrapper">
-
-		<?php if (get_theme_mod('pzl_hmenu_show') == 'Yes') : ?>
-			<?php if (get_theme_mod('pzl_hmenu_position') == 'Before') : ?>
-				<?php echo do_shortcode( get_theme_mod("pzl_hmenu_shortcode") ); ?>
-			<?php endif; ?>
-		<?php endif; ?>
-
-
-		<?php if (get_theme_mod('pzl_head_show') == 'Yes') : ?>
-			<?php echo do_shortcode( get_theme_mod("pzl_head_shortcode") ); ?>
-		<?php endif; ?>
-
-
-		<?php if (get_theme_mod('pzl_hmenu_show') == 'Yes') : ?>
-			<?php if (get_theme_mod('pzl_hmenu_position') == 'After') : ?>
-				<?php echo do_shortcode( get_theme_mod("pzl_hmenu_shortcode") ); ?>
-			<?php endif; ?>
-		<?php endif; ?>
-
-	</div><!-- header-wrapper -->
-
 	<div <?php post_class(); ?> >
