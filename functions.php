9df49fe0dbfbd901875440143327c8965fae1380
Luiz Eduardo de Oliveira Fonseca <luiz@powertic.com>
2018-06-25 11:07:51 -0300

    bump 1.1.6

--- functions.php
+++ functions.php
@@ -1,13 +1,4 @@
 <?php
-/*
-Theme Name: Elementor Puzzle
-Theme URI: https://luizeof.com.br
-Author: luizeof
-Author URI: http://luizeof.com.br
-Description: Elementor Puzzle Theme for Landing Pages and Websites
-Version: 1.1.5
-License: GNU General Public License
-*/
 
 require_once "customizer.php";
 
@@ -18,14 +9,14 @@ add_theme_support( 'title-tag' );
 add_theme_support( "post-thumbnails" );
 
 function register_landingpress_menus() {
-    register_nav_menus(
-      array(
-        'header-menu' => __( 'Header Menu' ),
-        'side-menu' => __( 'Side Menu' ),
-        'footer-menu' => __( 'Footer Menu' )
-      )
-    );
-  }
+  register_nav_menus(
+    array(
+      'header-menu' => __( 'Header Menu' ),
+      'side-menu' => __( 'Side Menu' ),
+      'footer-menu' => __( 'Footer Menu' )
+    )
+  );
+}
 add_action( 'init', 'register_landingpress_menus' );
 
 // Run updater
