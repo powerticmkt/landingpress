c7ee90b38dbdac0966433b60a7db2becf654a87e
Luiz Eduardo de Oliveira Fonseca <luiz@powertic.com>
2018-06-25 10:50:17 -0300

    Adicionando suporte ao Menu

--- functions.php
+++ functions.php
@@ -17,6 +17,17 @@ add_theme_support( 'title-tag' );
 
 add_theme_support( "post-thumbnails" );
 
+function register_landingpress_menus() {
+    register_nav_menus(
+      array(
+        'header-menu' => __( 'Header Menu' ),
+        'side-menu' => __( 'Side Menu' ),
+        'footer-menu' => __( 'Footer Menu' )
+      )
+    );
+  }
+add_action( 'init', 'register_landingpress_menus' );
+
 // Run updater
 require 'plugin-update-checker/plugin-update-checker.php';
 $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
